8928487cf35baf72836603d146f3c01ab751b823
Cody Morley <codymorleyinc@gmail.com>
2020-03-16 14:41:52 -0700

    added VocabularyWord.swift

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,15 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Cody Morley on 3/16/20.
+//  Copyright © 2020 Cody. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+    
+}
